8af7d37c26bcf4c078a5b57d46463d8e1c8035e8
Giselle Hoekveld <gisellehoekveld@gmail.com>
2023-01-04 00:31:26 -0300

    test: show user profile

---
+++ src/modules/users/useCases/showUserProfile/ShowUserProfileUseCase.spec.ts
@@ -0,0 +1,31 @@
+import { makeUser } from "../../../../test/factories/user-factory"
+import { InMemoryUsersRepository } from "../../repositories/in-memory/InMemoryUsersRepository"
+import { CreateUserUseCase } from "../createUser/CreateUserUseCase"
+import { ShowUserProfileError } from "./ShowUserProfileError"
+import { ShowUserProfileUseCase } from "./ShowUserProfileUseCase"
+
+let showUserProfileUseCase: ShowUserProfileUseCase
+let createUserUseCase: CreateUserUseCase
+let inMemoryUsersRepository: InMemoryUsersRepository
+
+describe("Show User Profile", () => {
+  beforeEach(() => {
+    inMemoryUsersRepository = new InMemoryUsersRepository()
+    showUserProfileUseCase = new ShowUserProfileUseCase(inMemoryUsersRepository)
+    createUserUseCase = new CreateUserUseCase(inMemoryUsersRepository)
+  })
+
+  it("should be able to show user profile", async () => {
+    const user = await createUserUseCase.execute(makeUser())
+
+    const profile = await showUserProfileUseCase.execute(user.id as string)
+
+    expect(profile).toHaveProperty("id")
+  })
+
+  it("should not be able to show profile of a non existing user", async () => {
+    expect(async() => 
+      await showUserProfileUseCase.execute("non-existing-user-id")
+    ).rejects.toBeInstanceOf(ShowUserProfileError)
+  })
+})
